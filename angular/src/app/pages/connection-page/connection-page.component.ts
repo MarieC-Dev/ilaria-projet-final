324f9fab47d73c796d3f01fb27a4f0e2b2a07068
MarieC-Dev <marie.costa13@gmail.com>
2025-04-01 10:49:49 +0200

    add connection page

---
+++ angular/src/app/pages/connection-page/connection-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-connection-page',
+  imports: [],
+  templateUrl: './connection-page.component.html',
+  styleUrl: './connection-page.component.scss'
+})
+export class ConnectionPageComponent {
+
+}

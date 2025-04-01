3448a5a51b90d6677ee6b950ee349b70624be29c
MarieC-Dev <marie.costa13@gmail.com>
2025-04-01 10:54:47 +0200

    add style url

--- angular/src/app/components/page-slider/page-slider.component.ts
+++ angular/src/app/components/page-slider/page-slider.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-page-slider',
   imports: [],
-  templateUrl: './page-slider.component.html'
+  templateUrl: './page-slider.component.html',
+  styleUrl: '../../../styles/style.scss'
+  
 })
 export class PageSliderComponent {
 
--- angular/src/app/components/recipe-item/recipe-item.component.ts
+++ angular/src/app/components/recipe-item/recipe-item.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-recipe-item',
   imports: [],
-  templateUrl: './recipe-item.component.html'
+  templateUrl: './recipe-item.component.html',
+  styleUrl: '../../../styles/style.scss'
 })
 export class RecipeItemComponent {
 

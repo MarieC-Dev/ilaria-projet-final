4407ccf385d7a62ecdbf9f75c93dbefdecb1ce6d
MarieC-Dev <marie.costa13@gmail.com>
2025-04-01 10:51:12 +0200

    add page slider

---
+++ angular/src/app/components/page-slider/page-slider.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PageSliderComponent } from './page-slider.component';
+
+describe('PageSliderComponent', () => {
+  let component: PageSliderComponent;
+  let fixture: ComponentFixture<PageSliderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PageSliderComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PageSliderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ angular/src/app/components/page-slider/page-slider.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-slider',
+  imports: [],
+  templateUrl: './page-slider.component.html'
+})
+export class PageSliderComponent {
+
+}

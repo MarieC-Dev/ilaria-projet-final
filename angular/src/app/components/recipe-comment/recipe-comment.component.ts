ee7a8b94304e4fc5b63ea61fcead1cd589833f67
MarieC-Dev <marie.costa13@gmail.com>
2025-04-02 18:34:54 +0200

    add comment component

---
+++ angular/src/app/components/recipe-comment/recipe-comment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-comment',
+  imports: [],
+  templateUrl: './recipe-comment.component.html',
+  styleUrl: './recipe-comment.component.scss'
+})
+export class RecipeCommentComponent {
+
+}

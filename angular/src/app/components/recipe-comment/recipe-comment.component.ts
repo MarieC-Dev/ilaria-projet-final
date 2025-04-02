8791fd029cb1ca435d673621becdf88ffc2d534b
MarieC-Dev <marie.costa13@gmail.com>
2025-04-02 20:57:34 +0200

    recipe comment is finished

--- angular/src/app/components/recipe-comment/recipe-comment.component.ts
+++ angular/src/app/components/recipe-comment/recipe-comment.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-comment',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './recipe-comment.component.html',
   styleUrl: './recipe-comment.component.scss'
 })
 export class RecipeCommentComponent {
+  picture = input<string>('');
+  username = input<string>('');
+  date = input<Date>();
+  note = input<number>();
+  text = input<string>('');
 
+  getStars(solid: boolean) {
+    const lastChar: string = String(this.note).charAt(String(this.note).length - 2);
+    const nb: number = parseInt(lastChar);
+    
+    if(nb > 0 && nb <= 5) {
+      const array = [];
+
+      for(let i = 0; i < nb; i++) {
+        array.push(`${i}`);
+      }
+
+      if(solid) {
+        return array;
+      } else { 
+        return array.splice(0, 5 - array.length);
+      }
+    } else {
+      return;
+    }
+  }
+
+  /*
+    Si la note est entre 1 et 5 -> 
+      - nb x <img src="star.svg" alt="star icon">
+      - reste avec <img src="star-border.svg" alt="star icon">
+    (switch ?)
+    Sinon -> ...
+  */
 }

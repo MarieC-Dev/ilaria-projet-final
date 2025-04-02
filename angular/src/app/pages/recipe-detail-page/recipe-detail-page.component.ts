2e50407ec0110346fd8e876d487f7dad10f40e88
MarieC-Dev <marie.costa13@gmail.com>
2025-04-02 13:01:46 +0200

    start layout recipe detail page

--- angular/src/app/pages/recipe-detail-page/recipe-detail-page.component.ts
+++ angular/src/app/pages/recipe-detail-page/recipe-detail-page.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RECIPE_LIST } from '../../lists/recipe-list.fake';
+import { JsonPipe } from '@angular/common';
+import { RecipeItemTimeComponent } from '../../components/layout-elements/recipe-item-time/recipe-item-time.component';
+
 
 @Component({
   selector: 'app-recipe-detail-page',
-  imports: [],
+  imports: [JsonPipe, RecipeItemTimeComponent],
   templateUrl: './recipe-detail-page.component.html',
   styleUrl: './recipe-detail-page.component.scss'
 })
 export class RecipeDetailPageComponent {
-
+  recipeList = signal(RECIPE_LIST);
+  recipe = this.recipeList()[0];
 }

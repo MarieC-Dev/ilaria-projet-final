575bea3c296cc1b5ae62b5e862163e6082a7411c
MarieC-Dev <marie.costa13@gmail.com>
2025-04-02 13:42:13 +0200

    add recipe step component

---
+++ angular/src/app/components/recipe-step/recipe-step.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-step',
+  imports: [],
+  templateUrl: './recipe-step.component.html',
+  styleUrl: './recipe-step.component.scss'
+})
+export class RecipeStepComponent {
+
+}

fdde8fafb32d555023678722beba970d8f683a87
MarieC-Dev <marie.costa13@gmail.com>
2025-04-10 10:39:36 +0200

    change class name

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
-import { RecipeDetailPageComponent } from './pages/recipe-detail-page/recipe-detail-page.component';
+import { RecipeDetailsPage } from './pages/recipe-details-page/recipe-details-page.component';
 import { SigninPageComponent } from './pages/signin-page/signin-page.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 export const routes: Routes = [
-    /* { path: '/recipe/:id', component: RecipeDetailPageComponent }, */
+    /* { path: '/recipe/:id', component: RecipeDetailsPage }, */
     { path: 'accueil', component: HomePageComponent },
-    { path: 'recettes', component: RecipeDetailPageComponent },
+    { path: 'recettes', component: RecipeDetailsPage },
     { path: 'connexion', component: SigninPageComponent },
     { path: 'inscription', component: SignupPageComponent },
     { path: 'contact', component: ContactPageComponent },

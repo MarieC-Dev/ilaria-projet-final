78ddf08c8bde9e77f2e22616ebce536a15454e6a
MarieC-Dev <marie.costa13@gmail.com>
2025-04-15 15:37:05 +0200

    creation of profile page

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -6,10 +6,12 @@ import { SigninPageComponent } from './pages/signin-page/signin-page.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { CreateRecipesComponent } from './pages/create-recipes/create-recipes.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 
 export const routes: Routes = [
     /* { path: '/recipe/:id', component: RecipeDetailsPage }, */
     { path: 'profil/creer-une-recette', component: CreateRecipesComponent },
+    { path: 'profil', component: ProfilePageComponent },
     { path: 'accueil', component: HomePageComponent },
     { path: 'recettes', component: RecipeDetailsPage },
     { path: 'connexion', component: SigninPageComponent },

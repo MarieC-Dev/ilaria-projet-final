d9fd3cc2df9ec7075e496a91f170d216cd73a921
MarieC-Dev <marie.costa13@gmail.com>
2025-04-16 13:30:06 +0200

    add search icon and create component icon

---
+++ angular/src/app/components/icons/chevron-down-icon/chevron-down-icon.component.ts
@@ -0,0 +1,10 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-chevron-down-icon',
+  imports: [],
+  templateUrl: './chevron-down-icon.component.html'
+})
+export class ChevronDownIconComponent {
+  style = input();
+}
---
+++ angular/src/app/components/icons/colors.ts
@@ -0,0 +1,27 @@
+const black: string = '#010101';
+const darkGrey: string = '#5A5A5A';
+const mediumGrey: string = '#848484';
+const lightGrey: string = '#F4F4F4';
+const lightBrown: string = '#F9F6F5';
+const firstColor: string = '#EB4A37';
+const secondColor: string = 'orange';
+const thirdColor: string = '#ffcc00';
+const facebook: string = '#0866ff';
+const linkedin: string = '#0a66c2';
+const xTwitter: string = 'black';
+const google: string = '#F23B35';
+
+export const COLORS = {
+    black,
+    darkGrey,
+    mediumGrey,
+    lightGrey,
+    lightBrown,
+    firstColor,
+    secondColor,
+    thirdColor,
+    facebook,
+    linkedin,
+    xTwitter,
+    google
+}

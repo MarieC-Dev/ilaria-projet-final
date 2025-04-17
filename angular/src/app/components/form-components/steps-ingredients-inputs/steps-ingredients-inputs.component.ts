51402eebcb1a8b96b277129bb16756db7449487c
MarieC-Dev <marie.costa13@gmail.com>
2025-04-17 10:31:55 +0200

    add steps ingredients lists

--- angular/src/app/components/form-components/steps-ingredients-inputs/steps-ingredients-inputs.component.ts
+++ angular/src/app/components/form-components/steps-ingredients-inputs/steps-ingredients-inputs.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MultipleInputsComponent } from '../multiple-inputs/multiple-inputs.component';
 import { CommonModule } from '@angular/common';
+import { DeleteIconComponent } from "../../icons/delete-icon/delete-icon.component";
 
 @Component({
   selector: 'app-steps-ingredients-inputs',
-  imports: [MultipleInputsComponent, CommonModule],
+  imports: [MultipleInputsComponent, CommonModule, DeleteIconComponent],
   templateUrl: './steps-ingredients-inputs.component.html',
   styleUrl: './steps-ingredients-inputs.component.scss'
 })

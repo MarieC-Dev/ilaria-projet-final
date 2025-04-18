afb2c341252c253e02ddb55c6fa8a40b6d3e5e6a
MarieC-Dev <marie.costa13@gmail.com>
2025-04-18 09:57:16 +0200

    create edit recipe : add cuisine type and change styles

--- angular/src/app/components/create-edit-recipe-form/create-edit-recipe-form.component.ts
+++ angular/src/app/components/create-edit-recipe-form/create-edit-recipe-form.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CookingTypeComponent } from '../form-components/cooking-type/cooking-type.component';
 import { DifficultyComponent } from '../form-components/difficulty/difficulty.component';
 import { FormInputComponent } from '../form-components/form-input/form-input.component';
 import { InputsTimesComponent } from '../form-components/inputs-times/inputs-times.component';
 import { MultipleInputsComponent } from '../form-components/multiple-inputs/multiple-inputs.component';
 import { StepsIngredientsInputsComponent } from '../form-components/steps-ingredients-inputs/steps-ingredients-inputs.component';
+import { CUISINE_TYPE } from '../../lists/cuisine-type-list';
 
 @Component({
   selector: 'app-create-edit-recipe-form',
@@ -14,11 +15,11 @@ import { StepsIngredientsInputsComponent } from '../form-components/steps-ingred
     InputsTimesComponent, 
     StepsIngredientsInputsComponent, 
     CookingTypeComponent, 
-    DifficultyComponent
+    DifficultyComponent,
   ],
   templateUrl: './create-edit-recipe-form.component.html',
   styleUrl: './create-edit-recipe-form.component.scss'
 })
 export class CreateEditRecipeFormComponent {
-
+  cuisineTypeList = signal(CUISINE_TYPE);
 }
---
+++ angular/src/app/lists/cuisine-type-list.ts
@@ -0,0 +1,57 @@
+let index: number = 0;
+
+export const CUISINE_TYPE = [
+    {
+        id: index++,
+        value: 'french',
+        name: 'Française'
+    }, {
+        id: index++,
+        value: 'italian',
+        name: 'Italienne'
+    }, {
+        id: index++,
+        value: 'spanish',
+        name: 'Espagnole'
+    }, {
+        id: index++,
+        value: 'asiatic',
+        name: 'Asiatique'
+    }, {
+        id: index++,
+        value: 'indian',
+        name: 'Indienne'
+    }, {
+        id: index++,
+        value: 'european',
+        name: 'Européenne'
+    }, {
+        id: index++,
+        value: 'north-african',
+        name: 'Maghrébine'
+    }, {
+        id: index++,
+        value: 'african',
+        name: 'Africaine'
+    }, {
+        id: index++,
+        value: 'grecian',
+        name: 'Grecque'
+    }, {
+        id: index++,
+        value: 'north-american',
+        name: 'Nord américaine'
+    }, {
+        id: index++,
+        value: 'north-american',
+        name: 'Sud américaine'
+    }, {
+        id: index++,
+        value: 'russian',
+        name: 'Russe'
+    }, {
+        id: index++,
+        value: 'australian',
+        name: 'Australienne'
+    }, 
+]

e60a8b6301684034ce1385c9ded296c081313fb6
MarieC-Dev <marie.costa13@gmail.com>
2025-04-21 18:26:53 +0200

    recipe filter animation

--- angular/src/app/directives/show-recipes-filter.directive.ts
+++ angular/src/app/directives/show-recipes-filter.directive.ts
@@ -14,19 +14,19 @@ export class ShowRecipesFilterDirective implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    const getItemList: HTMLElement = this.document.querySelector(`#${this.id} .navFiltersListItem`)!;
-    console.log(getItemList.offsetHeight);
-    
+    const getItemList: HTMLElement = this.document.querySelector(`#${this.id} .navFiltersListItem`)!;    
     this.renderer.setStyle(getItemList, 'max-height', '0');
   }
 
   @HostListener('click') onClick() {
-    const getItemList: HTMLElement = this.document.querySelector(`#${this.id} .navFiltersListItem`)!;
-
-    if(getItemList.offsetHeight === 2) {
-      this.renderer.setStyle(getItemList, 'max-height', 'max-content');      
+    const getItemList: HTMLElement = this.document.querySelector(`#${this.id} .navFiltersListItem`)!;    
+        
+    if(getItemList.offsetHeight === 0) {
+      this.renderer.setStyle(getItemList, 'max-height', `${getItemList.scrollHeight}px`);   
+      this.renderer.setStyle(getItemList, 'transition', `max-height ${0.4 * getItemList.childNodes.length}s ease-in-out`);
     } else {
       this.renderer.setStyle(getItemList, 'max-height', '0');     
+      this.renderer.setStyle(getItemList, 'transition', `max-height ${0.4 * getItemList.childNodes.length}s ease-in-out`);
     }    
   }
 }

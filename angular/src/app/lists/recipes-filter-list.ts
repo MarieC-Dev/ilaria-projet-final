03a80e3afed00b21d3df85356a169903f3b35544
MarieC-Dev <marie.costa13@gmail.com>
2025-04-21 18:03:36 +0200

    add recipe filter list

---
+++ angular/src/app/lists/recipes-filter-list.ts
@@ -0,0 +1,38 @@
+import { COOKING_TYPE_LIST } from "./cooking-type-list";
+import { CUISINE_TYPE } from "./cuisine-type-list";
+
+let index: number = 0;
+
+export const RECIPES_FILTER = [
+    {
+        id: index++,
+        title: 'Type de cuisine',
+        items: CUISINE_TYPE
+    }, {
+        id: index++,
+        title: 'Difficultés',
+        items: [
+            {
+                id: 0,
+                title: 'Ne sais pas',
+                value: 'dont-know',
+            }, {
+                id: 1,
+                title: 'Facile',
+                value: 'easy',
+            }, {
+                id: 2,
+                title: 'Moyen',
+                value: 'medium',
+            }, {
+                id: 3,
+                title: 'Difficile',
+                value: 'difficult',
+            }, 
+        ]
+    }, {
+        id: index++,
+        title: 'Type de cuisson',
+        items: COOKING_TYPE_LIST
+    }
+]

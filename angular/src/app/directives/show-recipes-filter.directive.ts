59fdbc5863c08581bedfacc5447e866a4edacade
MarieC-Dev <marie.costa13@gmail.com>
2025-04-21 19:04:14 +0200

    end of recipe filter animation

--- angular/src/app/directives/show-recipes-filter.directive.ts
+++ angular/src/app/directives/show-recipes-filter.directive.ts
@@ -20,13 +20,18 @@ export class ShowRecipesFilterDirective implements OnInit {
 
   @HostListener('click') onClick() {
     const getItemList: HTMLElement = this.document.querySelector(`#${this.id} .navFiltersListItem`)!;    
+    const plusIcon: HTMLElement = this.document.querySelector(`#${this.id} .verticalLine`)!;
+    console.log(plusIcon);
+    
         
     if(getItemList.offsetHeight === 0) {
       this.renderer.setStyle(getItemList, 'max-height', `${getItemList.scrollHeight}px`);   
+      this.renderer.setStyle(plusIcon, 'transform', `rotate(0deg)`);   
       this.renderer.setStyle(getItemList, 'transition', `max-height ${0.4 * getItemList.childNodes.length}s ease-in-out`);
     } else {
       this.renderer.setStyle(getItemList, 'max-height', '0');     
       this.renderer.setStyle(getItemList, 'transition', `max-height ${0.4 * getItemList.childNodes.length}s ease-in-out`);
+      this.renderer.setStyle(plusIcon, 'transform', `rotate(-90deg)`); 
     }    
   }
 }

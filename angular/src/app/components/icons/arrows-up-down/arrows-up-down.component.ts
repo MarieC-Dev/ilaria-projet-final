618bdc5639d760c50a770f3ea277cd9116bb3e53
MarieC-Dev <marie.costa13@gmail.com>
2025-04-25 10:13:58 +0200

    add arrows up down icon and component

---
+++ angular/src/app/components/icons/arrows-up-down/arrows-up-down.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-arrows-up-down',
+  imports: [],
+  templateUrl: './arrows-up-down.component.html',
+})
+export class ArrowsUpDownComponent {
+
+}

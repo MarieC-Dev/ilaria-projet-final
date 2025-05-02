15487c67f0172631721af633ab7751e3c0a9c3eb
MarieC-Dev <marie.costa13@gmail.com>
2025-05-02 12:42:03 +0200

    menu burger

--- angular/src/app/directives/burger-menu.directive.ts
+++ angular/src/app/directives/burger-menu.directive.ts
@@ -1,43 +1,116 @@
 import { DOCUMENT } from '@angular/common';
-import { Directive, HostListener, Inject, Input, OnInit, Renderer2 } from '@angular/core';
+import { computed, Directive, ElementRef, EventEmitter, HostListener, Inject, Input, OnInit, Output, Renderer2, signal } from '@angular/core';
 
 @Directive({
   selector: '[appBurgerMenu]'
 })
 export class BurgerMenuDirective {
-  @Input() burger: 'burger' = 'burger';
-  private burgerState: boolean = false;
+  //@Input('appBurgerMenu') btnType: 'burgerBtn' | 'burgerNav' = 'burgerBtn';
+  @Input() isOpen: boolean = false;
+  @Output() isOpenChange = new EventEmitter<boolean>();
 
-  constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) { }
+  constructor(
+    private renderer: Renderer2, 
+    @Inject(DOCUMENT) private document: Document,
+    private elm: ElementRef
+  ) {
+    console.log(this.isOpen);
+  }
 
-  @HostListener('click') onClick() {
+  // $event -> click
+  @HostListener('click', ['$event']) onClickBurger(event: MouseEvent) {
+    event.stopPropagation();
+  }
+
+  // on click on target (HTML element)
+  @HostListener('click', ['$event.target']) onClickNavLink(target: HTMLElement) { 
+    const linkTag = target.tagName === 'A' ? true : false;
+
+    if(this.isOpen && linkTag) {
+      this.isOpen = false;
+      this.isOpenChange.emit(this.isOpen);
+      this.closeMenu();
+    }
+  }
+
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+
+    console.log(this.isOpen);
+
+    if(this.isOpen) {
+      this.openMenu();
+    } else {
+      this.closeMenu();
+    }
+  }
+
+  openMenu() {
     const header: HTMLElement = this.document.querySelector('header')!;
     const menu: HTMLElement = this.document.querySelector('header .navigationLeft')!;
-    const burgerBtn: HTMLElement = this.document.querySelector('.burgerMenu')!;
     const firstLine: HTMLElement = this.document.querySelector('.burgerMenu span:first-child')!;
     const secondLine: HTMLElement = this.document.querySelector('.burgerMenu span:last-child')!;
 
-    if(this.burger === 'burger') {
-      this.burgerState = !this.burgerState;
-    
-      if(this.burgerState) {
-        this.renderer.setStyle(header, 'bottom', '0');
-        this.renderer.setStyle(menu, 'max-height', menu.scrollHeight + 144 + 'px');
-        this.renderer.setStyle(menu, 'padding', '3rem 1rem 6rem 1rem');
+    this.renderer.setStyle(header, 'bottom', '0');
+    this.renderer.setStyle(menu, 'max-height', menu.scrollHeight + 144 + 'px');
+    this.renderer.setStyle(menu, 'padding', '3rem 1rem 6rem 1rem');
 
-        this.renderer.setStyle(firstLine, 'transform', 'rotate(45deg) translateY(13px)');
-        this.renderer.setStyle(secondLine, 'transform', 'rotate(-45deg) translateY(-12px)');
-      } else {
-        this.renderer.setStyle(header, 'bottom', 'initial');
-        this.renderer.setStyle(menu, 'max-height', '0');
-        this.renderer.setStyle(menu, 'padding', '0');
-        this.renderer.setStyle(firstLine, 'transform', 'rotate(0)');
-        this.renderer.setStyle(secondLine, 'transform', 'rotate(0)');
-      }
-
-      console.log(this.burgerState);
-    }
-    
-    
+    this.renderer.setStyle(firstLine, 'transform', 'rotate(45deg) translateY(13px)');
+    this.renderer.setStyle(secondLine, 'transform', 'rotate(-45deg) translateY(-12px)');
   }
+
+  closeMenu() {
+    const header: HTMLElement = this.document.querySelector('header')!;
+    const menu: HTMLElement = this.document.querySelector('header .navigationLeft')!;
+    const firstLine: HTMLElement = this.document.querySelector('.burgerMenu span:first-child')!;
+    const secondLine: HTMLElement = this.document.querySelector('.burgerMenu span:last-child')!;
+
+    setTimeout(() => {
+      this.renderer.setStyle(header, 'bottom', 'initial');
+    }, 600);
+    
+    this.renderer.setStyle(menu, 'max-height', '0');
+    this.renderer.setStyle(menu, 'padding', '0');
+    this.renderer.setStyle(firstLine, 'transform', 'rotate(0)');
+    this.renderer.setStyle(secondLine, 'transform', 'rotate(0)');
+
+    this.isOpen = false;
+  }
+
+  /* toggleMenu() {
+    const header: HTMLElement = this.document.querySelector('header')!;
+    const menu: HTMLElement = this.document.querySelector('header .navigationLeft')!;
+    const firstLine: HTMLElement = this.document.querySelector('.burgerMenu span:first-child')!;
+    const secondLine: HTMLElement = this.document.querySelector('.burgerMenu span:last-child')!;
+
+    console.log(this.burgerState());
+
+    this.burgerState.update((bool) => bool = !bool)
+
+    if(this.burgerState()) {
+      this.openMenu(header, menu, firstLine, secondLine);
+    } else {
+      this.closeMenu(header, menu, firstLine, secondLine);
+    }
+  }
+
+  openMenu(header: HTMLElement, menu: HTMLElement, firstLine: HTMLElement, secondLine: HTMLElement) {
+    this.renderer.setStyle(header, 'bottom', '0');
+    this.renderer.setStyle(menu, 'max-height', menu.scrollHeight + 144 + 'px');
+    this.renderer.setStyle(menu, 'padding', '3rem 1rem 6rem 1rem');
+
+    this.renderer.setStyle(firstLine, 'transform', 'rotate(45deg) translateY(13px)');
+    this.renderer.setStyle(secondLine, 'transform', 'rotate(-45deg) translateY(-12px)');
+  }
+
+  closeMenu(header: HTMLElement, menu: HTMLElement, firstLine: HTMLElement, secondLine: HTMLElement) {
+    setTimeout(() => {
+      this.renderer.setStyle(header, 'bottom', 'initial');
+    }, 600);
+    
+    this.renderer.setStyle(menu, 'max-height', '0');
+    this.renderer.setStyle(menu, 'padding', '0');
+    this.renderer.setStyle(firstLine, 'transform', 'rotate(0)');
+    this.renderer.setStyle(secondLine, 'transform', 'rotate(0)');
+  } */
 }
--- angular/src/app/layout/main-layout/main-layout.component.ts
+++ angular/src/app/layout/main-layout/main-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ViewChild } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { SocialNetworksComponent } from '../../components/social-networks/social-networks.component';
 import { commonSocial } from '../../lists/social-networks-list';
@@ -19,5 +19,27 @@ import { BurgerMenuDirective } from '../../directives/burger-menu.directive';
   styleUrl: '../../../styles.scss'
 })
 export class MainLayoutComponent {
+  @ViewChild(BurgerMenuDirective) appBurgerMenu!: BurgerMenuDirective;
   socialNetworksList = signal(commonSocial);
+
+  index: number = 0;
+  headerNav = [
+    {
+      id: this.index++,
+      link: '/accueil',
+      name: 'Accueil'
+    }, {
+      id: this.index++,
+      link: '/recettes',
+      name: 'Recettes'
+    }, {
+      id: this.index++,
+      link: '/rechercher',
+      name: 'Rechercher'
+    }, {
+      id: this.index++,
+      link: '/contact',
+      name: 'Contact'
+    }, 
+  ];
 }

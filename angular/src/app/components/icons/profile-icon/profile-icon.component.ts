9614903dc7ab1348c61e598072fe6961abdb2b42
MarieC-Dev <marie.costa13@gmail.com>
2025-05-02 16:22:30 +0200

    add icons components

---
+++ angular/src/app/components/icons/profile-icon/profile-icon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-icon',
+  imports: [],
+  templateUrl: './profile-icon.component.html'
+})
+export class ProfileIconComponent {
+
+}
---
+++ angular/src/app/components/icons/recipe-icon/recipe-icon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-icon',
+  imports: [],
+  templateUrl: './recipe-icon.component.html'
+})
+export class RecipeIconComponent {
+
+}

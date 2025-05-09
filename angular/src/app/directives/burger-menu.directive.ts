50b820d64f98bdf61296108a83efa6489bad7dee
MarieC-Dev <marie.costa13@gmail.com>
2025-05-09 18:51:06 +0200

    remove console log

--- angular/src/app/directives/burger-menu.directive.ts
+++ angular/src/app/directives/burger-menu.directive.ts
@@ -10,12 +10,10 @@ export class BurgerMenuDirective {
   @Output() isOpenChange = new EventEmitter<boolean>();
 
   constructor(
-    private renderer: Renderer2, 
+    private renderer: Renderer2,
     @Inject(DOCUMENT) private document: Document,
     private elm: ElementRef
-  ) {
-    console.log(this.isOpen);
-  }
+  ) { }
 
   @HostListener('click', ['$event']) onClickBurger(event: MouseEvent) {
     event.stopPropagation();
@@ -54,7 +52,7 @@ export class BurgerMenuDirective {
     setTimeout(() => {
       this.renderer.setStyle(header, 'bottom', 'initial');
     }, 600);
-    
+
     this.renderer.setStyle(menu, 'max-height', '0');
     this.renderer.setStyle(menu, 'padding', '0');
     this.renderer.setStyle(firstLine, 'transform', 'rotate(0)');
@@ -62,41 +60,4 @@ export class BurgerMenuDirective {
 
     this.isOpen = false;
   }
-
-  /* toggleMenu() {
-    const header: HTMLElement = this.document.querySelector('header')!;
-    const menu: HTMLElement = this.document.querySelector('header .navigationLeft')!;
-    const firstLine: HTMLElement = this.document.querySelector('.burgerMenu span:first-child')!;
-    const secondLine: HTMLElement = this.document.querySelector('.burgerMenu span:last-child')!;
-
-    console.log(this.burgerState());
-
-    this.burgerState.update((bool) => bool = !bool)
-
-    if(this.burgerState()) {
-      this.openMenu(header, menu, firstLine, secondLine);
-    } else {
-      this.closeMenu(header, menu, firstLine, secondLine);
-    }
-  }
-
-  openMenu(header: HTMLElement, menu: HTMLElement, firstLine: HTMLElement, secondLine: HTMLElement) {
-    this.renderer.setStyle(header, 'bottom', '0');
-    this.renderer.setStyle(menu, 'max-height', menu.scrollHeight + 144 + 'px');
-    this.renderer.setStyle(menu, 'padding', '3rem 1rem 6rem 1rem');
-
-    this.renderer.setStyle(firstLine, 'transform', 'rotate(45deg) translateY(13px)');
-    this.renderer.setStyle(secondLine, 'transform', 'rotate(-45deg) translateY(-12px)');
-  }
-
-  closeMenu(header: HTMLElement, menu: HTMLElement, firstLine: HTMLElement, secondLine: HTMLElement) {
-    setTimeout(() => {
-      this.renderer.setStyle(header, 'bottom', 'initial');
-    }, 600);
-    
-    this.renderer.setStyle(menu, 'max-height', '0');
-    this.renderer.setStyle(menu, 'padding', '0');
-    this.renderer.setStyle(firstLine, 'transform', 'rotate(0)');
-    this.renderer.setStyle(secondLine, 'transform', 'rotate(0)');
-  } */
 }

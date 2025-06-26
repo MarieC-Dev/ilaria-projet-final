9b11744b29085df2a07ae83841351f053307b4fb
MarieC-Dev <marie.costa13@gmail.com>
2025-06-26 22:22:40 +0200

    add comment form group factory

---
+++ angular/src/app/factories/comment-form.factory.ts
@@ -0,0 +1,14 @@
+import {Injectable} from '@angular/core';
+import {FormGroup, FormArray, Validators, FormControl} from '@angular/forms';
+
+@Injectable({ providedIn: 'root' })
+export class RecipeFormFactory {
+  formGroup: FormGroup;
+
+  constructor() {
+    this.formGroup = new FormGroup({
+      note: new FormControl<number>(0),
+      commentText: new FormControl<string>('')
+    });
+  }
+}

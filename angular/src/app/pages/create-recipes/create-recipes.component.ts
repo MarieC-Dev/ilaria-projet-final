2905377ed157a29ab97492ca116c9b7ece92cdde
MarieC-Dev <marie.costa13@gmail.com>
2025-06-26 16:57:20 +0200

    create recipe page- go back button

--- angular/src/app/pages/create-recipes/create-recipes.component.ts
+++ angular/src/app/pages/create-recipes/create-recipes.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { CreateEditRecipeFormComponent } from '../../components/create-edit-recipe-form/create-edit-recipe-form.component';
+import {Location} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-create-recipes',
-  imports: [CreateEditRecipeFormComponent],
+  imports: [CreateEditRecipeFormComponent, RouterLink],
   templateUrl: './create-recipes.component.html',
   styleUrl: './create-recipes.component.scss'
 })
 export class CreateRecipesComponent {
+  constructor(private location: Location) { }
 
+  goBack() {
+    this.location.back();
+  }
 }

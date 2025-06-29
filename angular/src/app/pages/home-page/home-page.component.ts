f93fc3bb3dea339577be01377b5ef8ef614be2ce
MarieC-Dev <marie.costa13@gmail.com>
2025-06-29 17:21:26 +0200

    show all recipes

--- angular/src/app/pages/home-page/home-page.component.ts
+++ angular/src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import { YouTubePlayer } from '@angular/youtube-player';
 
 import { PageSliderComponent } from '../../components/page-slider/page-slider.component';
@@ -10,6 +10,8 @@ import { SearchIconComponent } from "../../components/icons/search-icon/search-i
 import { SortBySelectComponent } from "../../components/sort-by-select/sort-by-select.component";
 import { VideosSliderDirective } from '../../directives/videos-slider.directive';
 import { PlayIconComponent } from "../../components/icons/play-icon/play-icon.component";
+import {RecipesApiService} from '../../services/recipes-api.service';
+import {JsonPipe} from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
@@ -22,16 +24,28 @@ import { PlayIconComponent } from "../../components/icons/play-icon/play-icon.co
     VideosSliderDirective,
     YouTubePlayer,
     PlayIconComponent,
+    JsonPipe
 ],
   templateUrl: './home-page.component.html',
   styleUrl: '../../../styles.scss'
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
   recipesList = signal(RECIPE_LIST);
+  getAllRecipes: any[] = [];
   recipeAverage = inject(RecipeAverageService);
 
   index: number = 0;
-  
+
+  constructor(private recipeApi: RecipesApiService) {
+  }
+
+  ngOnInit(): void {
+    this.recipeApi.getAllRecipes().subscribe({
+      next: (result) => this.getAllRecipes = result,
+      error: (err) => console.log(err),
+    });
+  }
+
   getRecipeAverage(id: number) {
     return this.recipeAverage.getRecipeAverage(id, this.recipesList());
   }

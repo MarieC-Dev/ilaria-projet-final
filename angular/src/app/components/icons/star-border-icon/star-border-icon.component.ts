5eb64979a038c143abfa6076e545a1b4b78fbd6d
MarieC-Dev <marie.costa13@gmail.com>
2025-06-29 17:28:58 +0200

    commit

---
+++ angular/src/app/components/icons/star-border-icon/star-border-icon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-star-border-icon',
+  imports: [],
+  templateUrl: './star-border-icon.component.html'
+})
+export class StarBorderIconComponent {
+
+}

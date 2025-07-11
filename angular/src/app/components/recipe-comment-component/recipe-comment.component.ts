53f86066530c98975d808c78a53421089f797834
MarieC-Dev <marie.costa13@gmail.com>
2025-07-11 13:31:48 +0200

    comment component : number of stars

--- angular/src/app/components/recipe-comment-component/recipe-comment.component.ts
+++ angular/src/app/components/recipe-comment-component/recipe-comment.component.ts
@@ -12,7 +12,7 @@ import {UsersApiService} from '../../services/users-api.service';
 export class RecipeCommentComponent implements OnInit {
   userId = input<number>();
   date = input<string>();
-  note = input<number>();
+  note = input<number>(1);
   text = input<string>('');
   answersArray = input<number>(0);
   showAnswers = model(false); // input + output for change value
@@ -31,7 +31,25 @@ export class RecipeCommentComponent implements OnInit {
     })
   }
 
-  getStars(solid: boolean) {
+  getStars(note: any) {
+    const stars: boolean[] = [];
+    const noteNb = Number(note);
+
+    for (let i = 0; i < note; i++) {
+      console.log(i)
+      stars.push(true);
+    }
+
+    if(note < 5) {
+      for (let i = 0; i < 5 - note; i++) {
+        stars.push(false);
+      }
+    }
+
+    return stars;
+  }
+
+  /*getStars(solid: boolean) {
     const lastChar: string = String(this.note).charAt(String(this.note).length - 2);
     const nb: number = parseInt(lastChar);
 
@@ -50,5 +68,5 @@ export class RecipeCommentComponent implements OnInit {
     } else {
       return;
     }
-  }
+  }*/
 }

5e6509aae9c640685a3d77960218d04415997441
MarieC-Dev <marie.costa13@gmail.com>
2025-07-16 12:55:43 +0200

    add heart border icon component

---
+++ angular/src/app/components/icons/heart-border-icon/heart-border-icon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heart-border-icon',
+  imports: [],
+  templateUrl: './heart-border-icon.component.html'
+})
+export class HeartBorderIconComponent {
+
+}

d3ebc01921dfa5c680ef6c1b9839134c53c35fe1
MarieC-Dev <marie.costa13@gmail.com>
2025-08-21 15:25:13 +0200

    signin error message

--- angular/src/app/pages/signin-page/signin-page.component.ts
+++ angular/src/app/pages/signin-page/signin-page.component.ts
@@ -4,44 +4,39 @@ import { connectionSocial } from '../../lists/social-networks-list';
 import {UserFormFactory} from '../../factories/user-form.factory';
 import {ReactiveFormsModule} from '@angular/forms';
 import {UsersApiService} from '../../services/users-api.service';
-import {IsLoggedInService} from '../../services/isLoggedIn.service';
-import {JsonPipe} from '@angular/common';
-import {Router} from '@angular/router';
 import {AuthStateService} from '../../services/auth-state.service';
 
 @Component({
   selector: 'app-signin-page',
-  imports: [SocialNetworksComponent, ReactiveFormsModule, JsonPipe],
+  imports: [SocialNetworksComponent, ReactiveFormsModule],
   templateUrl: './signin-page.component.html',
   styleUrl: './signin-page.component.scss'
 })
 export class SigninPageComponent implements OnInit {
   socialNetworksList = signal(connectionSocial);
   userLogin!: UserFormFactory;
-  isLoggedInData = {};
+  loginError: boolean = false;
 
   constructor(
-    private router: Router,
     private usersApiService: UsersApiService,
-    private isLoggedIn: IsLoggedInService,
     private authState: AuthStateService
   ) { }
 
   ngOnInit(): void {
     this.userLogin = new UserFormFactory();
-    /*this.isLoggedIn.isLoggedIn().subscribe(isLoggedIn => {
-      console.log(isLoggedIn);
-      this.isLoggedInData = isLoggedIn;
-    });*/
   }
 
   onSubmit() {
     this.usersApiService.login(this.userLogin.formGroupLogin.value).subscribe({
       next: (res) => {
         console.log(res);
+        this.loginError = false;
         this.authState.login(res.user);
       },
-      error: (err) => console.log('Login front error ', err),
+      error: (err) => {
+        console.log('Login front error ', err);
+        this.loginError = true;
+      },
     });
   }
 }
